8b3cd1d57af56eb3fff8d53273386f879033ac39
victorbfcastro <victorbfc@hotmail.com>
2020-12-10 15:32:15 -0300

    Primeiro commit

--- BecaMatriz/Program.cs
+++ BecaMatriz/Program.cs
@@ -10,9 +10,55 @@ namespace BecaMatriz
     {
         static void Main(string[] args)
         {
-            // Cada Becario deverá criar um metódo para calcular a soma da diagonal principal de uma matriz quadrada 3 x 3 
-            // O método deverá ter o nome do participante
-            // exemplo: CalculaSomaDiagonalPrincipal_Ricardo();
+            int[,] matriz = new int[3, 3];
+            SomaDiagonal_Victor(matriz);
+        }
+
+        // Victor Castro
+
+        private static void ImprimeMatriz(int[,] matriz)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"{matriz[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        private static void SomaDiagonal_Victor(int[,] matriz)
+        {
+            for (int i = 0; i < 3; i++)     //Preenche a lista com dados do usuario
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"Valor para a posição [{i},{j}]: ");
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            int[,] matrizPronta = matriz;
+
+            int soma = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (i == j)
+                    {
+                        soma += matriz[i, j];
+                    }
+                }
+            }
+
+            ImprimeMatriz(matrizPronta);
+
+            Console.WriteLine($"\nSoma da Diagonal: {soma}");
+            Console.Read();
         }
     }
+
 }
