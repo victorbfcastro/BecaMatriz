0202d60d6769d605ae8d6a914b6c0fb1b36bd045
ricanascimento <ricardo.paulino.nascimento@prestadoresuhg.com.br>
2020-12-10 15:03:13 -0300

    init

--- BecaMatriz/Program.cs
+++ BecaMatriz/Program.cs
@@ -10,6 +10,9 @@ namespace BecaMatriz
     {
         static void Main(string[] args)
         {
+            // Cada Becario deverá criar um metódo para calcular a soma da diagonal principal de uma matriz quadrada 3 x 3 
+            // O método deverá ter o nome do participante
+            // exemplo: CalculaSomaDiagonalPrincipal_Ricardo();
         }
     }
 }
